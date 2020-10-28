246910677f941a025acffd109d5680240df49c51
ShkurinaMaria <shkurina64@gmail.com>
2020-10-28 22:31:14 +0500

    Fix Main8 and Main10

--- src/tasks1/begin/Main10.java
+++ src/tasks1/begin/Main10.java
@@ -7,6 +7,6 @@ public class Main10 {
         System.out.println("Сумма чисел " + a + " и " + b + " = " + (a + b));
         System.out.println("Разность чисел " + a + " и " + b + " = " + (a - b));
         System.out.println("Произведение чисел " + a + " и " + b + " = " + (a * b));
-        System.out.println("Частное модулей чисел " + a + " и " + b + " = " + Math.abs(a / b));
+        System.out.println("Частное модулей чисел " + a + " и " + b + " = " + (a * a) / (b * b));
     }
 }
--- src/tasks1/begin/Main8.java
+++ src/tasks1/begin/Main8.java
@@ -1,11 +1,9 @@
 package tasks1.begin;
 
-import jdk.jshell.Snippet;
-
 public class Main8 {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         float a = 22.33f;
         float b = 10.776f;
-        System.out.println("Среднее арифметическое чисел " + a + " и " + b + " = " + (a + b)/2);
+        System.out.println("Среднее арифметическое чисел " + a + " и " + b + " = " + (a + b) / 2);
     }
 }
