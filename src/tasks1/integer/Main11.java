909de293b72a69bcfcb33d9cd8d99d2b8be2bea1
ShkurinaMaria <shkurina64@gmail.com>
2020-10-31 23:23:46 +0500

    Resolve

---
+++ src/tasks1/integer/Main11.java
@@ -0,0 +1,12 @@
+package tasks1.integer;
+
+public class Main11 {
+    public static void main(String[] args) {
+        int a = 153;
+        int w = a % 10;
+        int v = a / 10;
+        int x = v % 10;
+        int c = a / 100;
+        System.out.println("Сумма цифр числа " + a + " = " + (w + x + c));
+    }
+}
