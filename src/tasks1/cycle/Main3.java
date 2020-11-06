fef222327d0f4fa817c72fd28408c4897dcebd00
ShkurinaMaria <shkurina64@gmail.com>
2020-11-06 17:32:38 +0500

    Fix

--- src/tasks1/cycle/Main3.java
+++ src/tasks1/cycle/Main3.java
@@ -4,11 +4,9 @@ public class Main3 {
     public static void main(String[] args) {
         int a = 18;
         int b = 26;
-        int a1 = a + 1;
-        int b1 = b - 1;
-        for (int i = a1; i < b1; i++) {
+        for (int i = b; i > a; i--) {
             System.out.println("Число " + i);
         }
-        System.out.println("Количество чисел = " + (b1 - a1));
+        System.out.println("Количество чисел = " + (b - a));
     }
 }
