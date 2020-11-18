7f3c56660a59887bf25900bae1e86f25ec6415b9
ShkurinaMaria <shkurina64@gmail.com>
2020-11-18 20:31:11 +0500

    Fix

--- src/tasks1/series/Main23.java
+++ src/tasks1/series/Main23.java
@@ -2,37 +2,36 @@ package tasks1.series;
 
 public class Main23 {
     public static void main(String[] args) {
-        Side2 side2 = new Side2(new int[]{2, 6,4,5,4,5});
-        side2.comp();
-        System.out.println(side2.getResult());
+        System.out.println(new Side2(new int[]{4, 6, 4}).comp() == 0);
+        System.out.println(new Side2(new int[]{6, 4, 6}).comp() == 0);
+        System.out.println(new Side2(new int[]{0, 0, 0}).comp() == 1);
+        System.out.println(new Side2(new int[]{1, 2, 3}).comp() == 1);
+        System.out.println(new Side2(new int[]{4, 6, 4, 3, 5}).comp() == 2);
+        System.out.println(new Side2(new int[]{6, 7, 3}).comp() == 0);
     }
 }
 
 class Side2 {
     int[] arr = {};
-    int result = 0;
 
     public Side2(int[] arr) {
         this.arr = arr;
     }
 
-    void comp() {
-        int i = 0;
-        boolean isPila = (arr[i] == arr[i + 1]);
-        if (!isPila) {
-            for (int j = 1; j < arr.length - 1; j++) {
-                if (arr[j] > arr[j - 1] && arr[j] > arr[j + 1] || arr[j] < arr[j - 1] && arr[j] < arr[j + 1]) {
-                    result += 0;
-                } else {
-                    result = arr[j];
-                }
-            }
-        } else {
-            result = 1;
+    int comp() {
+        if (arr[0] == arr[1]) {
+            return 1;
         }
-    }
-
-    public int getResult() {
-        return result;
+        if (arr[arr.length - 1] == arr[arr.length - 2]) {
+            return arr[arr.length - 2];
+        }
+        for (int j = 1; j < arr.length - 1; j++) {
+            boolean firstCondition = arr[j] > arr[j - 1] && arr[j] < arr[j + 1];
+            boolean secondCondition = arr[j] < arr[j - 1] && arr[j] > arr[j + 1];
+            if (firstCondition || secondCondition) {
+                return j;
+            }
+        }
+        return 0;
     }
 }
