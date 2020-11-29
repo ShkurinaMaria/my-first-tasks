cd53105946d2622283d4e3366cfcc68200005e59
ShkurinaMaria <shkurina64@gmail.com>
2020-11-29 17:32:15 +0500

    Fix

--- src/array/Main5.java
+++ src/array/Main5.java
@@ -2,14 +2,14 @@ package array;
 
 public class Main5 {
     public static void main(String[] args) {
-        int[] arr = generateArr(5, 1, 1);
+        int[] arr = generateArr(5);
         print(arr);
     }
 
-    static int[] generateArr(int n, int f1, int f2) {
+    static int[] generateArr(int n) {
         int[] arr = new int[n];
-        arr[0] = f1;
-        arr[1] = f2;
+        arr[0] = 1;
+        arr[1] = 1;
         for (int i = 2; i < n; i++) {
             arr[i] = arr[i - 1] + arr[i - 2];
         }
