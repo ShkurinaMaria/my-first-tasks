33a043ed892d8d36ae6e4850821ab331bc37f17a
ShkurinaMaria <shkurina64@gmail.com>
2020-11-29 19:44:46 +0500

    Fix

--- src/array/Main9.java
+++ src/array/Main9.java
@@ -9,19 +9,21 @@ public class Main9 {
     static int[] generateArr(int n) {
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = i;
+            arr[i] = i + 3;
         }
         return arr;
     }
 
     static void print(int[] arr) {
+        int count = 0;
 
         for (int j = arr.length - 1; j >= 0; j--) {
             if (arr[j] % 2 == 0) {
+                count++;
                 System.out.print(arr[j] + " ");
             }
-
         }
+        System.out.print("Количество = " + count);
     }
 }
 
