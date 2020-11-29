d3e54436bd87bfda59a49acc489f9ef56808c237
ShkurinaMaria <shkurina64@gmail.com>
2020-11-29 17:59:21 +0500

    Fix

--- src/array/Main8.java
+++ src/array/Main8.java
@@ -2,7 +2,7 @@ package array;
 
 public class Main8 {
     public static void main(String[] args) {
-        int[] arr = generateArr(5);
+        int[] arr = generateArr(9);
         print(arr);
     }
 
