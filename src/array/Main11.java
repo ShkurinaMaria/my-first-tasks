2b18c9cd63fe5532179a7b1695e81be63f0f2dc3
ShkurinaMaria <shkurina64@gmail.com>
2020-11-29 17:00:27 +0500

    decided

---
+++ src/array/Main11.java
@@ -0,0 +1,27 @@
+package array;
+
+public class Main11 {
+    public static void main(String[] args) {
+        int[] arr = generateArr(10);
+        print(arr);
+    }
+
+    static int[] generateArr(int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i;
+        }
+        return arr;
+    }
+
+    static void print(int[] arr) {
+        int k = 3;
+        int idx = 0;
+        for (int i = 0, j = k; j < arr.length; j += k, i++) {
+            idx = j;
+            System.out.print("Элемент = " + idx + " ");
+            System.out.println("Индекс = " + j + " ");
+        }
+    }
+}
+
