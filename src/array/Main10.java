d9e9008aaf07f33a37f9fd816c5ab5a6c5a96d5f
ShkurinaMaria <shkurina64@gmail.com>
2020-12-01 18:00:07 +0500

    Fix

--- src/array/Main10.java
+++ src/array/Main10.java
@@ -1,10 +1,8 @@
 package array;
 
-import java.util.Arrays;
-
 public class Main10 {
     public static void main(String[] args) {
-        int[] arr = generateArr(10);
+        int[] arr = generateArr(9);
         print(arr);
     }
 
@@ -17,11 +15,14 @@ public class Main10 {
     }
 
     static void print(int[] arr) {
-        for (int j = 0; j < arr.length; j += 2) {
-            System.out.print(arr[j] + " ");
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
         }
-        for (int j = arr.length - 1; j > 0; j -= 2) {
-            System.out.print(arr[j] + " ");
-        }
+        for (int i = arr.length - 1; i >= 0; i--)
+            if (arr[i] % 2 != 0) {
+                System.out.print(arr[i] + " ");
+            }
     }
 }
