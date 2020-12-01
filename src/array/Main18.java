7062e4bb18d799b50eb770d6865ba859a5cc8452
ShkurinaMaria <shkurina64@gmail.com>
2020-12-01 19:52:12 +0500

    decided

--- src/array/Main18.java
+++ src/array/Main18.java
@@ -2,15 +2,13 @@ package array;
 
 public class Main18 {
     public static void main(String[] args) {
-        int[] arr = {11, 2, 3, 4, 5, 6, 7, 8, 9, 20};
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] < arr[9]) {
-                System.out.println(arr[i]);
-                break;
-            } else {
-                System.out.println(" 0 ");
-                break;
+        int[] arr = {11, 11, 11, 10};
+        int digit = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < arr[3]) {
+                digit = arr[i];
             }
         }
+        System.out.println(digit);
     }
 }
