969e9e6fe2192ebe31d6fe2043328b192b03b85b
ShkurinaMaria <shkurina64@gmail.com>
2020-12-01 18:36:29 +0500

    Fix

--- src/array/Main10.java
+++ src/array/Main10.java
@@ -15,11 +15,12 @@ public class Main10 {
     }
 
     static void print(int[] arr) {
-        for (int i = 0; i <= arr.length - 1; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 0) {
                 System.out.print(arr[i] + " ");
             }
         }
+        System.out.println();
         for (int i = arr.length - 1; i >= 0; i--)
             if (arr[i] % 2 != 0) {
                 System.out.print(arr[i] + " ");
