5cd6c90e2d39090b923eb4ac6bf6337222f83d06
ShkurinaMaria <shkurina64@gmail.com>
2020-12-01 19:59:52 +0500

    decided

---
+++ src/array/Main19.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Main19 {
+    public static void main(String[] args) {
+        int[] arr = {98, 22, 33, 4, 55, 66, 99, 88, 9, 100};
+        int idx = 0;
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[0] < arr[i] && arr[i] < arr[9]) {
+                idx = i;
+            }
+        }
+        System.out.println(idx);
+    }
+}
