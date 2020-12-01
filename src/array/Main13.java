f77c74d0692d40367ea9f1ecb82c9ff5fbb25c17
ShkurinaMaria <shkurina64@gmail.com>
2020-12-01 18:41:02 +0500

    Fix

--- src/array/Main13.java
+++ src/array/Main13.java
@@ -2,24 +2,24 @@ package array;
 
 public class Main13 {
     public static void main(String[] args) {
-        int[] arr = generateArr(10);
+        int[] arr = generateArr(11);
         print(arr);
     }
 
     static int[] generateArr(int n) {
-        if (n % 2 != 0) {
+        if (n % 2 == 0) {
             throw new Error("Количество элементов массива должно быть нечетным");
         }
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = i * 8;
+            arr[i] = i;
         }
         return arr;
     }
 
     static void print(int[] arr) {
         for (int j = arr.length - 1; j > 0; j -= 2) {
-            System.out.print(arr[j] + " ");
+            System.out.print(arr[j - 1] + " ");
         }
     }
 }
