050a45a0894846b80c2a2c388d0ff3ffda938610
ShkurinaMaria <shkurina64@gmail.com>
2020-12-09 22:49:38 +0500

    fix

--- src/array/Main52.java
+++ src/array/Main52.java
@@ -1,18 +1,11 @@
 package array;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main52 {
     public static void main(String[] args) {
-        float[] arr = {1, 10, 10, 1, 1};
-        float result[] = new float[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 5) {
-                result[i] = 2 * arr[i];
-            } else {
-                result[i] = arr[i] / 2;
-            }
-        }
-        System.out.println(Arrays.toString(result));
+        List<Integer> arr = Arrays.asList(1, 10, 10, 1, 1);
+        System.out.println(Arrays.toString(arr.stream().mapToInt(el -> el < 5 ? el * 2 : el / 2).toArray()));
     }
 }
