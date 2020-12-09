9d9eae4dac32493498b825f555fd5985eeaa4817
ShkurinaMaria <shkurina64@gmail.com>
2020-12-09 21:42:30 +0500

    fix

--- src/array/Main56.java
+++ src/array/Main56.java
@@ -8,7 +8,7 @@ public class Main56 {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(5, 8, 9, 4, 2, 4, 4, 5, 9, 9, 9, 9, 9);
         List<Integer> list1 = new ArrayList<>();
-        for (int i = 3; i < list.size(); i += 3) {
+        for (int i = 0; i < list.size(); i += 3) {
             list1.add(list.get(i));
         }
         System.out.print(list1 + " " + list1.size());
