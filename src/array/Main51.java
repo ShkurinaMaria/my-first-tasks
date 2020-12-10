67a612c9858126f5bcd39d2988d3fab97c8e7560
ShkurinaMaria <shkurina64@gmail.com>
2020-12-10 13:54:46 +0500

    fix

--- src/array/Main51.java
+++ src/array/Main51.java
@@ -8,10 +8,12 @@ public class Main51 {
     public static void main(String[] args) {
         List<Integer> arrA = Arrays.asList(2, 2, 2, 2);
         List<Integer> arrB = Arrays.asList(27, 42, 67, 6);
-        List<Integer> result = new ArrayList<>(arrA);
-        arrA = arrB;
-        arrB = result;
+        for (int i = 0; i < arrA.size(); i++) {
+            int t = arrA.get(i);
+            arrA.set(i, arrB.get(i));
+            arrB.set(i, t);
+        }
         System.out.println(arrA);
-        System.out.println(result);
+        System.out.println(arrB);
     }
 }
