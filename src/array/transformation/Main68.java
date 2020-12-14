c3b05da809e8584632dd3cebd3cdb139e7fe620d
home_profile <shkurina64@gmail.com>
2020-12-14 20:09:34 +0300

    Fix

--- src/array/transformation/Main68.java
+++ src/array/transformation/Main68.java
@@ -4,14 +4,11 @@ import java.util.*;
 
 public class Main68 {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(3, 2, 2, 2, 4, 45, -10);
+        List<Integer> list = Arrays.asList(-63, 2, -2, 2, 4, 45, 10);
         int max = Collections.max(list);
         int min = Collections.min(list);
-        int temp = min;
-        min = max;
-        max = temp;
         list.set(list.indexOf(max), min);
-        list.set(list.indexOf(min), temp);
+        list.set(list.indexOf(min), max);
         System.out.print(list);
     }
 }
