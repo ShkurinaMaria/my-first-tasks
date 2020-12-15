afe92ec15f94e8dfc5765f31f5145b6bfadc21e3
ShkurinaMaria <shkurina64@gmail.com>
2020-12-15 17:02:54 +0500

    fix

--- src/Masha/Main111.java
+++ src/Masha/Main111.java
@@ -1,19 +1,25 @@
 package Masha;
 
-import java.util.Arrays;
-
 public class Main111 {
     public static void main(String[] args) {
-        String str = "dtvvmj@mail.ru";
-        String[] subStr = str.split("@");
-        String[] subStr1 = subStr[1].split("\\.");
-        for (int i = 0; i < subStr.length; i++) {
-            if (i == 0) {
-                System.out.print(subStr[0].charAt(0) + "" + subStr[0].charAt(1) + "" + subStr[0].charAt(2) + "***@");
-            } else {
-                System.out.print("***." + subStr1[1]);
-            }
+        String[] emails = {
+                "dtvvmj@mail.ru",
+                "acro221996@mail.ru",
+                "acro221996@yande.ru",
+                "acro221996@gmail.com",
+                "a@adf.onin"
+        };
+        for (String email : emails) {
+            System.out.println(getHiddenEmail(email));
         }
+    }
+
+    public static String getHiddenEmail(String email) {
+        String[] emailAndDomain = email.split("@");
+        String[] subStr1 = emailAndDomain[1].split("\\.");
+        return (emailAndDomain[0].length() > 2 ? emailAndDomain[0].substring(0, 3) : emailAndDomain[0]) +
+                "***@***." +
+                subStr1[1];
     }
 }
 
