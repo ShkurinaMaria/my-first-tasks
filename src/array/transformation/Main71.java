12f35cd281ff5197523b8573537d172d51301319
ShkurinaMaria <shkurina64@gmail.com>
2020-12-16 23:54:36 +0500

    resolved

---
+++ src/array/transformation/Main71.java
@@ -0,0 +1,13 @@
+package array.transformation;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main71 {
+    public static void main(String[] args) {
+        List<Integer> arr = Arrays.asList(22, 66, 78, 4, 8);
+        Collections.reverse(arr);
+        System.out.println(arr);
+    }
+}
