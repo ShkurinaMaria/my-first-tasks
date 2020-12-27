8f006b358c07e4b7f676979bfc62d68d05d122cc
home_profile <shkurina64@gmail.com>
2020-12-27 17:34:09 +0300

    Fix

--- src/array/matrix/Main15a.java
+++ src/array/matrix/Main15a.java
@@ -2,7 +2,7 @@ package array.matrix;
 
 public class Main15a {
     public static void main(String[] args) {
-        int[][] matrix = Util.generateRandomMatrix(6, 6);
+        int[][] matrix = Util.generateRandomMatrix(2, 2);
         Util.printMatrix(matrix);
         Main15a.printSpiral(matrix);
     }
@@ -12,25 +12,21 @@ public class Main15a {
         int l = 0, k = 0;
         int lastRows = matrix.length - 1;
         int lastColumns = matrix.length - 1;
-        while (k <= lastRows &&
-                l <= lastColumns) {
+        while (k <= lastRows && l <= lastColumns) {
             for (i = l; i <= lastColumns; i++) {
                 System.out.print(matrix[k][i] + "  ");
             }
             k++;
-            System.out.println();
             for (i = k; i <= lastRows; i++) {
                 System.out.print(matrix[i][lastColumns] + "  ");
             }
             lastColumns--;
-            System.out.println();
             if (k <= lastRows) {
                 for (i = lastColumns; i >= l; i--) {
                     System.out.print(matrix[lastRows][i] + "  ");
                 }
                 lastRows--;
             }
-            System.out.println();
             if (l <= lastColumns) {
                 for (i = lastRows; i >= k; i--) {
                     System.out.print(matrix[i][l] + "  ");
--- src/array/matrix/Main16.java
+++ src/array/matrix/Main16.java
@@ -2,7 +2,7 @@ package array.matrix;
 
 public class Main16 {
     public static void main(String[] args) {
-        int[][] matrix = Util.generateRandomMatrix(5, 5);
+        int[][] matrix = Util.generateRandomMatrix(3, 3);
         Util.printMatrix(matrix);
         Main16.printSpiral(matrix);
     }
@@ -18,17 +18,14 @@ public class Main16 {
                 System.out.print(matrix[i][firstColumns] + "  ");
             }
             firstColumns++;
-            System.out.println();
             for (i = firstColumns; i <= lastColumns; i++) {
                 System.out.print(matrix[lastRow][i] + "  ");
             }
             lastRow--;
-            System.out.println();
             for (i = lastRow; i >= firstRows; i--) {
                 System.out.print(matrix[i][lastColumns] + "  ");
             }
             lastColumns--;
-            System.out.println();
             for (i = lastColumns; i >= firstColumns; i--) {
                 System.out.print(matrix[firstRows][i] + "  ");
             }
