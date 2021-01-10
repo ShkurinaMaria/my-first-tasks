3014f9a8a92354ff2f47fd6b295d343f59caa1f7
home_profile <shkurina64@gmail.com>
2021-01-10 17:50:43 +0300

    result

---
+++ src/array/transformation1/Main58.java
@@ -0,0 +1,29 @@
+package array.transformation1;
+
+import array.matrix.Util;
+
+import java.util.Arrays;
+
+public class Main58 {
+    public static void main(String[] args) {
+        int[][] matrix = Util.generateRandomMatrix(4, 6);
+        Util.printMatrix(matrix);
+        Main58.chengeHalfRow(matrix, 4, 6);
+    }
+
+    static void chengeHalfRow(int[][] matrix, int rows, int columns) {
+        int countRow = 0, countColumns = columns / 2, digit = 0;
+        for (int i = rows / 2; i < rows; i++) {
+            for (int j = 0; j < columns / 2; j++) {
+                digit = matrix[i][j];
+                matrix[i][j] = matrix[countRow][j + countColumns];
+                matrix[countRow][j + countColumns] = digit;
+            }
+            if (countRow < rows / 2) {
+                countRow++;
+                countColumns = columns / 2;
+            }
+        }
+        System.out.println(Arrays.deepToString(matrix));
+    }
+}
