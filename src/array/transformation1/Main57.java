f3495c3ce8230b51fa61447a4b4c99f28381d96b
home_profile <shkurina64@gmail.com>
2021-01-10 17:46:48 +0300

    result

--- src/array/transformation1/Main57.java
+++ src/array/transformation1/Main57.java
@@ -12,16 +12,16 @@ public class Main57 {
     }
 
     static void chengeHalfRow(int[][] matrix, int rows, int columns) {
-        int count = columns / 2, count1 = columns / 2, digit = 0;
+        int countRow = rows / 2, countColumns = columns / 2, digit = 0;
         for (int i = 0; i < rows / 2; i++) {
             for (int j = 0; j < columns / 2; j++) {
                 digit = matrix[i][j];
-                matrix[i][j] = matrix[count][j + count1];
-                matrix[count][j + count] = digit;
+                matrix[i][j] = matrix[countRow][j + countColumns];
+                matrix[countRow][j + countColumns] = digit;
             }
-            if (count < columns - 1) {
-                count++;
-                count1 = columns / 2;
+            if (countRow < rows - 1) {
+                countRow++;
+                countColumns = columns / 2;
             }
         }
         System.out.println(Arrays.deepToString(matrix));
