13b7bd6d2f86b7d721ac1b7bdf4b7c171be87d7c
ShkurinaMaria <shkurina64@gmail.com>
2021-01-20 16:16:30 +0500

    resolved

--- src/array/transformation/Main75.java
+++ src/array/transformation/Main75.java
@@ -1,22 +1,14 @@
 package array.transformation;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main75 {
     public static void main(String[] args) {
-        List<Integer> arr = Arrays.asList(3, -99, 2, 12, 101, 8);
+        List<Integer> arr = Arrays.asList(31, 9, 2, 12, -101, 8);
         int idxMax = arr.indexOf(Collections.max(arr));
         int idxMin = arr.indexOf(Collections.min(arr));
-
-        if (idxMax > idxMin) {
-            List<Integer> beforeMaxMin = arr.subList(idxMin + 1, idxMax);
-            Collections.reverse(beforeMaxMin);
-        } else {
-            List<Integer> beforeMaxMin = arr.subList(idxMax + 1, idxMin);
-            Collections.reverse(beforeMaxMin);
-        }
+        List<Integer> beforeMaxMin = idxMax > idxMin ? arr.subList(idxMin, idxMax + 1) : arr.subList(idxMax, idxMin + 1);
+        Collections.reverse(beforeMaxMin);
         System.out.println(arr);
     }
 }
